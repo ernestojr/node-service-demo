eaba49ec5d5441879c97e7607fa76f8cc3aeba4d
Ernesto Rojas <ejrojasp@falabella.cl>
2022-12-12 22:23:03 -0300

    Updated PORT var

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 const app = express()
 
-const PORT = process.env.NODE_PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
