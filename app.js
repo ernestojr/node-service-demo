7361f30ececd8eca02453c7816a05435066b2a70
Ernesto Rojas <ejrojasp@falabella.cl>
2022-12-12 21:34:31 -0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import http from 'http'
+import path from 'path'
+
+const app = express()
+
+const PORT = process.env.NODE_PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join('./', 'public')))
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}/`)
+})
